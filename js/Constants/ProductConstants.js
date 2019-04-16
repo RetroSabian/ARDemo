4a7d249356b26193e694adf0b15b4f55cde8299c
Sabian <srajbully@retrorabbit.co.za>
2019-04-16 15:21:52 +0200

    Dynamic ViroImageMarker

--- js/Constants/ProductConstants.js
+++ js/Constants/ProductConstants.js
@@ -4,11 +4,13 @@ import KelloggsImage from '../res/Markers/SpecialK.jpg';
 export const ProductArray = [
     {
         name: 'Romany Creams',
+        concatName: 'Romany_Creams',
         price: 25.50,
         imageMarker: RomanyCreamsImage
     },
     {
         name: 'Special K Red Berries Cereal Bars',
+        concatName: 'Special_K',
         price: 30.00,
         imageMarker: KelloggsImage
     }
