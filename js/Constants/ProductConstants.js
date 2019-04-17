812c0e806ab05811cd8ec5dc65110e4cf93a8ce6
Sabian <srajbully@retrorabbit.co.za>
2019-04-17 09:30:10 +0200

    dynamic ViroARTrackingTargets

--- js/Constants/ProductConstants.js
+++ js/Constants/ProductConstants.js
@@ -6,12 +6,16 @@ export const ProductArray = [
         name: 'Romany Creams',
         concatName: 'Romany_Creams',
         price: 25.50,
-        imageMarker: RomanyCreamsImage
+        source: RomanyCreamsImage,
+        orientation: 'Up',
+        physicalWidth: 0.15
     },
     {
         name: 'Special K Red Berries Cereal Bars',
         concatName: 'Special_K',
         price: 30.00,
-        imageMarker: KelloggsImage
+        source: KelloggsImage,
+        orientation: 'Up',
+        physicalWidth: 0.18
     }
 ];
